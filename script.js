955cc165459cea3cd8d47f24b439f5eab5733763
ChaosHeart6 <snsdsunny159@gmail.com>
2025-06-25 13:45:06 +0800

    加入活動Banner_JS轉播圖功能

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("GODIVA 活動頁啟動!");
+
+const banner = document.getElementById("banner");
+const images = [
+    "images/banner01.jpg",
+    "images/banner02.jpg",
+    "images/banner03.png"
+];
+
+let index = 0;
+setInterval(() => {
+   index = (index + 1)%images.length;
+   banner.style.opacity = 0;
+    setTimeout(() => {
+        banner.src = images[index];
+        banner.style.opacity = 1; 
+    }, 900);
+}, 5000);
+
