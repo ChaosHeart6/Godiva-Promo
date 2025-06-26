f00fe72f6ff5ffc7d2cb7cac91d014d0b407ffdc
ChaosHeart6 <snsdsunny159@gmail.com>
2025-06-26 15:32:08 +0800

    新增網頁資訊、留言與MYSOL資料庫之間連結的功能

---
+++ save.php
@@ -0,0 +1,25 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "godiva_db";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+if($conn->connect_error){
+    die("連線失敗" . $conn->connect_error);
+}
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+$sql = "INSERT INTO messages(name , email , message) VALUES(? , ? , ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $name, $email, $message);
+$stmt->execute();
+
+echo "留言送出成功！<br>";
+echo "<a href='form.html'>回到表單</a>";
+
+$stmt->close();
+$conn->close();
+?>
