774b8baf1d497fb97e6fb9adfda6d3245c2fd99d
ChaosHeart6 <snsdsunny159.com>
2025-07-02 18:50:45 +0800

    新增線上與本機測試資料庫切換(gitignore沒有上傳)

---
+++ config_sample.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$user = "your_user";
+$password = "your_password";
+$dbname = "your_database";
+?>
+/*請自行填入並取代view和save的php檔2-6行*/
--- save.php
+++ save.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$dbname = "godiva_db";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once "config.php";
 
 $conn = new mysqli($host, $user, $password, $dbname);
 if($conn->connect_error){
